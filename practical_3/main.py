e60a7a498e42621db01fc46846d075c34af982e2
anveshdange <dangeanv@gmail.com>
2023-06-19 20:06:28 +0530

    Added driver code

--- practical_3/main.py
+++ practical_3/main.py
@@ -14,18 +14,21 @@ def sep() -> None :
     print(seperator)
     return None
 
-# List into numpy array 
-p : List[int] = [1,2,3,4,5] 
-p_n : Any = numpy.array(p , dtype=numpy.float64) 
-print(f"Array before: {p}")
-print(f"Numpy Converted: {p_n}") 
-sep() 
+# Driver Code 
+if __name__ == "__main__" :
+    
+    # List into numpy array 
+    p : List[int] = [1,2,3,4,5] 
+    p_n : Any = numpy.array(p , dtype=numpy.float64) 
+    print(f"Array before: {p}")
+    print(f"Numpy Converted: {p_n}") 
+    sep() 
 
-# Tuple into numpy array
-q : List[int] = (1,2,3,4,5)
-q_n : Any = numpy.array(q, dtype=numpy.float64) 
-print(f"Tuple before: {q}")
-print(f"Numpy Converted: {q_n}")
-sep()
+    # Tuple into numpy array
+    q : List[int] = (1,2,3,4,5)
+    q_n : Any = numpy.array(q, dtype=numpy.float64) 
+    print(f"Tuple before: {q}")
+    print(f"Numpy Converted: {q_n}")
+    sep()
 
-print("Program ran succesfully")
+    print("Program ran succesfully")
